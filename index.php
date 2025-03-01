4b126e5400d9606dfb17167716f1ad0b4b8be690
AnaliseeDesenvolvimentodeSistemas <you@example.com>
2025-03-01 14:50:58 -0300

    Arquivos01

---
+++ index.php
@@ -0,0 +1,68 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Projeto_Docker_Swarm_e_Kubernetes</title>
+    <link rel="stylesheet" href="CSS/estilos.css">
+</head>
+   <header>
+   <marquee> Projeto com o Curso Docker para Desenvolvedores (com Docker Swarm e Kubernetes) </marquee>
+   <p class="imagem">
+    <img src="/IMG/imagem01.webp" alt="">
+   </p>
+    
+   </header>
+<body>
+<div class="cabeca">
+       <nav>
+         <ul>
+            <a href="#"> PHP</a>
+             <a href="#">JS</a>
+             <a href="#">CSS</a>
+             <a href="#">HTML</a>
+         </ul>
+       </nav>
+     </div>
+     <div class="corpo">
+     Descrição: <br>
+      sobre O Curso  Docker para Desenvolvedores (com Docker Swarm e Kubernetes) 
+<p>
+Seja bem-vindo ao curso completo de Docker! Nele, vou te ensinar tudo sobre essa poderosa ferramenta, desde os conceitos básicos até as funcionalidades mais avançadas,
+ sem que você precise de nenhum conhecimento prévio. <br>
+Durante o curso, você aprenderá, tanto na teoria quanto na prática, a utilizar o Docker em diversos projetos e ambientes (PHP, JavaScript, Python, entre outros). 
+As aulas são projetadas para iniciantes, com uma abordagem prática que prepara você para o mercado de trabalho. <br>
+Alguns tópicos abordados incluem:
+
+</p>
+
+   <p>  Fluxos com containers
+    Criação e atualização de imagens
+    Disponibilização de imagens no Docker Hub
+    Criação e uso de Volumes e Bind Mounts
+    Redes no Docker e conexão entre containers
+    Docker Compose para gerenciamento de múltiplos containers
+    Vários projetos práticos
+    E muito mais! </p>
+    
+  <p>
+    Além disso, você aprenderá YAML, uma linguagem essencial para quem trabalha com Docker e Kubernetes.
+
+
+Com foco na utilização do terminal, você também terá uma seção exclusiva sobre comandos Linux, muito requisitados no mercado.
+Após dominar o Docker, vamos explorar orquestração de containers com Docker Swarm e Kubernetes, ferramentas indispensáveis para profissionais de DevOps.    <br>
+Ao concluir o curso, você será capaz de criar e gerenciar projetos Dockerizados com eficiência, além de ter experiência em orquestração com Swarm e Kubernetes!
+Pronto para dar o próximo passo na sua carreira? Vamos aprender Docker do zero!
+Última atualização - 06/11/2024
+  </p>
+
+
+     </div>
+</body>
+   <footer>
+    <div class="rodape">
+        Andre luis Kunert_Fevereiro / Abril 2025
+    </div>
+   </footer>
+   <marquee> Projeto com o Curso Docker para Desenvolvedores (com Docker Swarm e Kubernetes) </marquee>
+</html>
